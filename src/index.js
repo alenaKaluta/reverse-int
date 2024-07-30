bef01a0d66915e1dc6c4724d624980d15ecfd5cf
alenaKaliuta <lenkazasloveskya@gmail.com>
2024-07-30 23:33:53 +0300

    solve algorithmic task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const numString = Math.abs(n).toString();
+  const numArr = numString.split('');
+  const reverseArr = numArr.reverse();
+  const result = reverseArr.join('');
+  return +result;
 }
